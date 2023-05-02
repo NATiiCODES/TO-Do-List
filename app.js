8d7089bbddd8443bbea7fc9742e15338f5c4469a
natiicodes <natashaew88@gmail.com>
2023-05-02 10:40:10 -0700

    HTML, CSS and Javascript
    Made a to do list with color coded task that you can personally select to make sure all of your to do's are in a color you choose for that list. You can add any many as you like!

---
+++ app.js
@@ -0,0 +1,45 @@
+const todoInput = document.getElementById("todo-input");
+const addBtn = document.getElementById("add-btn");
+const clearBtn = document.getElementById("clear-btn");
+const todoList = document.getElementById("todo-list");
+
+addBtn.addEventListener("click", addTodo);
+todoInput.addEventListener("keyup", function(event) {
+  if (event.keyCode === 13) {
+    event.preventDefault();
+    addBtn.click();
+  }
+});
+clearBtn.addEventListener("click", clearList);
+todoList.addEventListener("change", toggleTodo);
+
+function addTodo() {
+  const todoText = todoInput.value.trim();
+  if (todoText !== "") {
+    const li = document.createElement("li");
+    const checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
+    checkbox.className = "checkbox";
+    const span = document.createElement("span");
+    span.innerText = todoText;
+    li.appendChild(checkbox);
+    li.appendChild(span);
+    li.style.backgroundColor = document.getElementById("color").value;
+    todoList.appendChild(li);
+    todoInput.value = "";
+  }
+}
+
+function clearList() {
+  todoList.innerHTML = "";
+}
+
+function toggleTodo(event) {
+  const checkbox = event.target;
+  const li = checkbox.parentElement;
+  if (checkbox.checked) {
+    li.classList.add("completed");
+  } else {
+    li.classList.remove("completed");
+  }
+}
